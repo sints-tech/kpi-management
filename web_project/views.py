3d26a2d31100b2d7588907fa492cca7c0ced78d1
darma04 <darmaajigunawan@gmail.com>
2025-12-12 00:23:22 +0700

    Fix: Update SystemView to properly set status code for error handlers

--- web_project/views.py
+++ web_project/views.py
@@ -6,6 +6,7 @@ from web_project.template_helpers.theme import TemplateHelper
 class SystemView(TemplateView):
     template_name = "pages/system/not-found.html"
     status = ""
+    status_code = 200
 
     def get_context_data(self, **kwargs):
         # A function to init the global layout. It is defined in web_project/__init__.py file
@@ -21,3 +22,23 @@ class SystemView(TemplateView):
         )
 
         return context
+
+    def render_to_response(self, context, **response_kwargs):
+        # Set status code from the view instance
+        response = super().render_to_response(context, **response_kwargs)
+        response.status_code = self.status_code
+        return response
+
+    @classmethod
+    def as_view(cls, **initkwargs):
+        # Extract status code from initkwargs and set it as an instance attribute
+        status_code = initkwargs.pop('status', 200)
+        view = super().as_view(**initkwargs)
+
+        def wrapped_view(request, *args, **kwargs):
+            response = view(request, *args, **kwargs)
+            if hasattr(response, 'status_code'):
+                response.status_code = status_code
+            return response
+
+        return wrapped_view
