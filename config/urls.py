61d24d6ebc5cf72d02570c0d85cd6e7ce016d89c
darma04 <darmaajigunawan@gmail.com>
2025-12-12 01:32:34 +0700

    Fix: Add HttpResponse import for test endpoint

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 from web_project.views import SystemView
 import logging
 
